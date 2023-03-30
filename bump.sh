c5918d613e91c3cb2a718f318818c9bb49867be7
mwrites <20499416+mwrites@users.noreply.github.com>
2023-03-31 05:18:35 +0700

    v1 1.5.0 and the rest 2.0.6

---
+++ bump.sh
@@ -0,0 +1,10 @@
+cargo set-version \
+--workspace \
+ --bump patch \
+    --exclude mat-clockwork-app \
+    --exclude clockwork_plugin \
+    --exclude mat-clockwork-relayer \
+    --exclude mat-clockwork-relayer-api \
+    --exclude publish-helper \
+    --exclude mat-clockwork-thread-program-v1
+
